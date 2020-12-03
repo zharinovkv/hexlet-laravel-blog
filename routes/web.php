a0a9819b2788719d859f42571dafc31b818ccc26
Konstantin Zharinov <zharinovkv@gmail.com>
2020-12-03 19:39:02 +0500

    Edit files

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,19 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| 
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('/welcome', function () {
+    return '<h1>welcome</h1>';
+});
+
+
